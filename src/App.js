346e1894c8bae889875c6d666540085cb55047ce
Paula_Gil <135mariapaula@gmail.com>
2021-07-15 00:15:01 -0500

    proyecto terminado

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import Side from "./Components/Side"
+import Actividad from "./Components/Actividad"
+import Up from "./Components/Up"
+import Down from "./Components/Down"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="barra_lado"><Side/></div>
+      <div className="parte_arriba"><Up/></div>
+      <div className="caja_actividad"><Actividad/></div>
+      <div className="parte_abajo"><Down/></div>
     </div>
   );
 }
